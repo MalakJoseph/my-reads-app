289cd453148d97b89a04e68c05f7baa94b8a4806
Malak Joseph <malak@wuilt.com>
2020-06-06 01:31:08 +0200

    Updates

---
+++ precache-manifest.107146a65a245f6e7a3c6ac73cfe0eef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979110999d8f63a41cbb9194ef9cd6bc",
+    "url": "/my-reads-app/index.html"
+  },
+  {
+    "revision": "75c58e73f2d0019a1fd6",
+    "url": "/my-reads-app/static/css/main.c257d3f0.chunk.css"
+  },
+  {
+    "revision": "2fe76ce9dcd65d815b8e",
+    "url": "/my-reads-app/static/js/2.2eb8b0a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-reads-app/static/js/2.2eb8b0a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c58e73f2d0019a1fd6",
+    "url": "/my-reads-app/static/js/main.7bacd5c8.chunk.js"
+  },
+  {
+    "revision": "51cf1b9964f317b822ca",
+    "url": "/my-reads-app/static/js/runtime-main.dc14c557.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/my-reads-app/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/my-reads-app/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-reads-app/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
